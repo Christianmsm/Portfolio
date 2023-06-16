87ac9b9c34d41a90c8a1dbc3c52ab6d4c92a915a
Christian Smith <christiansmith@Christians-MBP.lan>
2023-06-15 21:17:12 -0400

    Created a Home page file

--- portfolio/src/components/PortfolioContainer.js
+++ portfolio/src/components/PortfolioContainer.js
@@ -1,21 +1,24 @@
 import React, { useState } from 'react';
+import Home from './pages/Home';
 import Navigation from "./Navigation";
 import AboutMe from "./pages/About";
 import ContactForm from "./pages/ContactForm";
+import Footer from './Footer';
 
 export default function PortfolioContainer() {
     const [currentPage, setCurrentPage] = useState('Home')
 
     const renderPage = () => {
-        // if (currentPage === 'Home') {
-        //     return <Home />;
-        // }
+        if (currentPage === 'Home') {
+            return <Home />;
+        }
         if (currentPage === 'About') {
             return <AboutMe />
         }
         if (currentPage === 'Contact') {
             return <ContactForm />
-        }
+        } 
+        return <Footer />
     }
 
     const handlePageChange = (page) => setCurrentPage(page);
---
+++ portfolio/src/components/pages/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Home() {
+    return (
+        <div> 
+            <h1>Home</h1>
+        <div>
+            <h2>Hello, welcome to my portfolio page!</h2>
+        </div>
+        <div>
+            <h4>My name is Christian Smith and I am a Software Engineer. I got my education through The Ohio State University boot camp program. This program was 24 weeks of intense training covering various frameworks and technologies such as React, SQL, NodeJS, and much more. Through this program I have successfully deployed many different projects and applications using the knowledge I have gained. Feel free to browse around and if you have any questions reach out to me via my contact form or one of the various links and I look forward to chatting with you!</h4>
+        </div>
+        </div>
+    );
+}
