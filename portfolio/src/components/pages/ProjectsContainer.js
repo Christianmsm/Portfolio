f233caf48ba2a52d7e1ced5937afbb5d34ac9200
Christian Smith <christiansmith@Christians-MBP.lan>
2023-06-18 21:33:50 -0400

    removing some starter files

--- portfolio/src/components/pages/ProjectsContainer.js
+++ portfolio/src/components/pages/ProjectsContainer.js
@@ -6,6 +6,7 @@ import ComingSoon from '../../images/Coming-soon.png';
 
 export default function ProjectContainer() {
 
+
     return (
         <div className="flex flex-col gap-10 lg:flex-row lg:flex-wrap">
             <div className="shadow-lg p-10 rounded-xl basis-1/3 flex-1 " id="PutASpork">
@@ -48,33 +49,3 @@ export default function ProjectContainer() {
         </div>
     );
 }
-// const myProjects = [
-    //     {
-    //         title: 'PutASporkInIt',
-    //         description: 'From the screen to your plate, this site brings all the recipes from your favorite tv shows & movies.',
-    //         deployed: 'https://www.putasporkinit.co/',
-    //         repo: 'https://github.com/CodeByDex/PutASporkInIt',
-    //         image: PutASpork//Need to add images
-    //     },
-    //     {
-    //         title: 'Recipe Muse',
-    //         description: 'Struggling to figure out what to make for your next meal? Recipe muse has you covered!',
-    //         deployed: 'https://malachigamblin.github.io/Recipe-Muse//',
-    //         repo: 'https://github.com/malachigamblin/Recipe-Muse',
-    //         image: RecipeMuse//Need to add images
-    //     },
-    //     {
-    //         title: 'Weather Dashboard',
-    //         description: 'Planning a trip and need to know how to pack? Here is the weather dashboard of your dreams! See the forecast in any location you are traveling too.',
-    //         deployed: 'https://christianmsm.github.io/06-Server-Side-API-Weather-Dashboard-Challenge/',
-    //         repo: 'https://github.com/Christianmsm/06-Server-Side-API-Weather-Dashboard-Challenge',
-    //         image: WeatherDash//Need to add images
-    //     },
-    //     {
-    //         title: 'TBD',
-    //         description: 'TBD',
-    //         deployed: 'TBD',
-    //         repo: 'TBD',
-    //         image: ''//Need to add images
-    //     },
-    // ]
