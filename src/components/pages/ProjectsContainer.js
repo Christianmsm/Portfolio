89560ed9514be7ca77d46a7a8bbd2ca0cd6d884c
Christian Smith <christiansmith@Christians-MBP.lan>
2023-07-08 14:08:19 -0400

    added more projects to the projects container

--- src/components/pages/ProjectsContainer.js
+++ src/components/pages/ProjectsContainer.js
@@ -2,6 +2,9 @@ import React from "react";
 import PutASpork from '../../images/putasporkinit.co.png';
 import RecipeMuse from '../../images/Recipe-muse.png';
 import WeatherDash from '../../images/Weather-dashboard.png';
+import PwdGenerator from '../../images/Pwd-Generator.png';
+import DailyPlanner from '../../images/DailyPlanner.png';
+import CodingQuiz from '../../images/CodingQuiz.png';
 import ComingSoonHero from '../../images/Coming-soon-img.png';
 
 export default function ProjectContainer() {
@@ -43,6 +46,50 @@ export default function ProjectContainer() {
             
             <a href="https://christianmsm.github.io/06-Server-Side-API-Weather-Dashboard-Challenge/"><button className="block w-full rounded-md bg-blue-950 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Deployed Site</button></a>
             </div>
+            </div>
+
+
+            <div className="shadow-lg p-10 rounded-xl basis-1/3 flex-1 " id="PwdGenerator">
+            <img className="mx-auto w-12/12 h-12/12 rounded-lg object-cover" layout="responsive" src={PwdGenerator} alt="Password generator project" />
+            <div>
+            <p className=" text-lg py-5 leading-8 ml-10 mr-10 text-center mt-12 mb-12">Are you one of the many people that use the same few passwords for all of your different accounts? Well I have a solution for you! This password generator application will create a unique password with the specific information you give it.</p>
+
+            <a href="https://github.com/Christianmsm/03-Password-Generator">
+            <button className="mb-5 block w-full rounded-md bg-blue-950 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">GitHub</button></a>
+            
+            <a href="https://christianmsm.github.io/03-Password-Generator/"><button className="block w-full rounded-md bg-blue-950 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Deployed Site</button></a>
+            </div>
+            </div>
+
+
+            <div className="shadow-lg p-10 rounded-xl basis-1/3 flex-1 " id="DailyPlanner">
+            <img className="mx-auto w-12/12 h-12/12 rounded-lg object-cover" layout="responsive" src={DailyPlanner} alt="Daily planner application" />
+            <div>
+            <p className=" text-lg py-5 leading-8 ml-10 mr-10 text-center mt-12 mb-12">This is an application that allows you to schedule out your whole work day hour by hour so that you can stay on track and be more productive. This application allows you to save the various tasks that you input to reference later.</p>
+
+            <a href="https://github.com/Christianmsm/Daily-Work-Planner">
+            <button className="mb-5 block w-full rounded-md bg-blue-950 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">GitHub</button></a>
+            
+            <a href="https://christianmsm.github.io/Daily-Work-Planner/"><button className="block w-full rounded-md bg-blue-950 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Deployed Site</button></a>
+            </div>
+            </div>
+
+
+            <div className="shadow-lg p-10 rounded-xl basis-1/3 flex-1 " id="CodingQuiz">
+            <img className="mx-auto w-12/12 h-12/12 rounded-lg object-cover" layout="responsive" src={CodingQuiz} alt="Coding quiz application" />
+            <div>
+            <p className=" text-lg py-5 leading-8 ml-10 mr-10 text-center mt-12 mb-12">Think you can beat the high score? This quiz game has you answer a set of questions while racing the clock. Give the wrong answer, and lose time. Try to see what high score you can get.</p>
+
+            <a href="https://github.com/Christianmsm/04-Coding-Quiz">
+            <button className="mb-5 block w-full rounded-md bg-blue-950 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">GitHub</button></a>
+            
+            <a href="https://christianmsm.github.io/04-Coding-Quiz/"><button className="block w-full rounded-md bg-blue-950 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Deployed Site</button></a>
+            </div>
+            </div>
+
+
+            <div className="shadow-lg p-10 rounded-xl basis-1/3 flex-1 " id="TBD">
+            <img className="mx-auto w-12/12 h-12/12 rounded-lg object-cover" layout="responsive" src={ComingSoonHero} alt="TBD project" />
             </div>
 
 
