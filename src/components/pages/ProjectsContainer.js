7147be71c19b56ad243d7929f808b32544ac871a
Christian Smith <christiansmith@Christians-MBP.lan>
2023-07-13 19:21:15 -0400

    added OIODev project

--- src/components/pages/ProjectsContainer.js
+++ src/components/pages/ProjectsContainer.js
@@ -6,12 +6,28 @@ import PwdGenerator from '../../images/Pwd-Generator.png';
 import DailyPlanner from '../../images/DailyPlanner.png';
 import CodingQuiz from '../../images/CodingQuiz.png';
 import ComingSoonHero from '../../images/Coming-soon-img.png';
+import OIODev from '../../images/oiodev.png';
 
 export default function ProjectContainer() {
 
 
     return (
+        
         <div className="flex flex-col gap-10 lg:flex-row lg:flex-wrap">
+
+<div className="shadow-lg p-10 rounded-xl basis-1/3 flex-1 " id="OIODev">
+            <img className="mx-auto w-12/12 h-12/12 rounded-lg object-cover" layout="responsive" src={OIODev} alt="OIODev application" />
+            <div>
+            <p className=" text-lg py-5 leading-8 ml-10 mr-10 text-center mt-12 mb-12">Looking for a developer to help grow your business and bring to life the ideas you have? OIODev is here. What OIODev is, is a website where you can search for various developers that you can book a call and hire to create whatever project you, and your business may have.</p>
+
+            <a href="https://github.com/CodeByDex/OIODev">
+            <button className="mb-5 block w-full rounded-md bg-blue-950 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">GitHub</button></a>
+            
+            <a href="https://www.oiodev.com/"><button className="block w-full rounded-md bg-blue-950 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Deployed Site</button></a>
+            </div>
+            </div>
+
+
             <div className="shadow-lg p-10 rounded-xl basis-1/3 flex-1 " id="PutASpork">
             <img className="mx-auto w-12/12 h-12/12 rounded-lg object-cover" layout="responsive" src={PutASpork} alt="PutASporkInIt project" />
             <div>
